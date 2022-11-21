7132b08b15a492529368acdddc281e8a15bde3b0
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2022-11-21 18:19:46 +0530

    Update keep_alive.py

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, make_response, request
 from threading import Thread
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive"
+    response = make_response("<h1>Success</h1>")
+    response.status_code = 200
+    return response
 
 def run():
   app.run(host='0.0.0.0',port=8080)
